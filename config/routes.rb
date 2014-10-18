d95c624034d65a057cdf2f75429a634937add6da
anusha28 <anusha.vutukuru@gmail.com>
2014-10-18 12:30:06 -0700

    added routes comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # LK's first comment to Git
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
